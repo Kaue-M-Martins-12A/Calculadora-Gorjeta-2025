ccdba9d29bfdbb672846d40c5718cd3cd02315a2
kaue.marques.martins09 <kaue.marques.martins09@gmail.com>
2025-04-07 03:01:56 +0000

    logica do custom tip adicionada e logica de desmarcar o botão ao escrever no custom tip

--- main.js
+++ main.js
@@ -18,9 +18,17 @@ function receiveTipPercentageValue(value) {
 
     removeClassButtonSelected()
 
+    document.querySelector("#custom-tip").value = ""
+
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
 
+}
+
+function receivCustomTipPercentageValue() {
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+
+    removeClassButtonSelected()
 }
 
 function removeClassButtonSelected() {
